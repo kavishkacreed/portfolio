343ee5889aef6a65cf6495bb1b428cc63ed2f272
kavishkacreed <kavishkanadiranga@gmail.com>
2024-07-09 16:47:34 +0530

    wwww

--- pages/services/index.js
+++ pages/services/index.js
@@ -31,7 +31,7 @@ const Services = () => {
               exit='hidden'
               className='mb-4 max-w-[400px] mx-auto lg:mx-0'
             >
-              Consider me your friendly neighborhood IT wizard!  Whether you need a website built from scratch or your computer rescued from the digital abyss, I'm here to solve your tech troubles. Here's how I can help:
+              Consider me your friendly neighborhood IT wizard!  Whether you need a website built from scratch or your computer rescued from the digital abyss, I&aposm here to solve your tech troubles. Here's how I can help:
             </motion.p>
           </div>
 
